80f473759f436087e231a9c3e522eaa6f75f6033
Abdelrahman Abdelbaky <abderahmanmohammed99@gmail.com>
2024-03-28 23:26:46 +0300

    Updating the player class

--- bullet.cpp
+++ bullet.cpp
@@ -1,3 +1,52 @@
-#include "bullet.h"
+#include"bullet.h"
+#include <QGraphicsScene>
+#include <QTimer>
+#include <QList>
+#include <enemy.h>
+#include <player.h>
 
-bullet::bullet() {}
+Bullet::Bullet(Player *player) : QObject(), QGraphicsPixmapItem(), player(player) {
+    // ***  Setting the bullet's size ****
+ setPixmap(QPixmap(":/resources/img/bullet 3.png").scaled(50, 70));
+    // ***  Generating the Bullets automatically ****
+    QTimer *timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(move()));
+    timer->start(50);
+
+
+    QAudioOutput *enemySound = new QAudioOutput;
+    enemySound->setVolume(100);
+    chickenSound = new QMediaPlayer;
+    chickenSound->setAudioOutput(enemySound);
+    chickenSound->setSource(QUrl("qrc:/resources/img/rubber-chicken-squeak-toy-1-181416.mp3"));
+
+}
+
+
+// Move function is used to move the bullet upwards and handle the collision of the bullets with enemies
+void Bullet::move() {
+    // ***  Getting the colliding items with the Bullet ****
+    QList<QGraphicsItem *> colliding_items = collidingItems();
+    for (int i = 0; i < colliding_items.size(); i++) {
+        if (typeid(*colliding_items[i]) == typeid(Enemy)) {
+            scene()->removeItem(colliding_items[i]);
+            chickenSound->play();
+            scene()->removeItem(this);
+            player->increase(); // Call increase() from the Player object to increase score
+            delete this;
+            delete colliding_items[i];
+            return; // Exit the function after increasing the score
+        }
+    }
+
+
+
+
+
+    // ***  Moving the bullets upward ****
+    setPos(x(), y() -  10);
+    if (pos().y() + pixmap().height() < 0) {
+        scene()->removeItem(this);
+        delete this;
+}
+}
--- bullet.h
+++ bullet.h
@@ -1,10 +1,25 @@
+// bullet.h
 #ifndef BULLET_H
 #define BULLET_H
 
-class bullet
-{
+#include <QObject>
+#include <QGraphicsRectItem>
+#include <player.h> // Include the player header to access its class definition
+#include <QAudioOutput>
+#include <QMediaPlayer>
+
+class Bullet : public QObject, public QGraphicsPixmapItem {
+    Q_OBJECT
 public:
-    bullet();
+    Bullet(Player *player); // Constructor with a reference to the Player object
+
+public slots:
+    void move(); // Slot to move the bullet and handle collisions
+
+private:
+    Player *player; // Reference to the Player object
+    QAudioOutput *enemySound;
+    QMediaPlayer *chickenSound;
 };
 
-#endif // BULLET_H
+#endif // BULLET_Hss
