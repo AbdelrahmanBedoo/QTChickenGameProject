0954d94529147e52fb2d97657ada8e51481d5d0d
“Sama <samaemad.aucegypt.edu>
2024-03-28 22:15:22 +0200

    updating player.h file

--- player.h
+++ player.h
@@ -1,10 +1,38 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 
-class player
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QAudioOutput>
+#include <QMediaPlayer>
+
+class QGraphicsScene;
+class QTimer;
+class QGraphicsTextItem;
+
+class Player: public QObject, public QGraphicsPixmapItem
 {
+    Q_OBJECT
 public:
-    player();
+    Player(QGraphicsScene *scene);
+public:
+    void keyPressEvent(QKeyEvent * event);
+    void decrease();
+    void increase();
+
+
+public slots:
+    void createEnemy();
+
+
+private:
+    QGraphicsScene *scene; // Pointer to the scene
+    int health;
+    int score;
+    QGraphicsTextItem *healthText;
+    QGraphicsTextItem *scoreText;
+    QAudioOutput *bulletSoundOutput;
+    QMediaPlayer *bulletSound;
 };
 
 #endif // PLAYER_H
